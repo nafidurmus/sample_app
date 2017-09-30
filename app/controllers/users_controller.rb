4411a13632fa5900e6ff9a4a76411d2899308584
nafi durmus <nafidurmus07@gmail.com>
2017-09-30 22:06:15 +0300

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,34 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: :show
+
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+
+  def show
+  end
+
+  private
+  def user_params
+    params.require(:user).permit :name, :email, :password, :password_confirmation
+  end
+
+  def find_user
+    @user = User.find_by id: params[:id]
+    unless @user
+      flash[:danger] = "User could not be found! "
+      redirect_to root_path
+    end
   end
 end
